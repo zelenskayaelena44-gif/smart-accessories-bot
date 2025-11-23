aa73eaccda8114fadc5885b30466b610ca883838
zelenskayaelena44-gif <zelenskaya.elena44@gmail.com>
2025-11-23 22:43:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler
 async def start(update, context):
     await update.message.reply_text("Привет! Я Smart Accessories Bot.")
 
-app = Application.builder().token("ТОКЕН_СЮДА").build()
+app = Application.builder().token("8270132256:AAELg2DTV0qifcHN4q8uvqrEFy6O75yMPcc").build()
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling(8270132256:AAELg2DTV0qifcHN4q8uvqrEFy6O75yMPcc)
+app.run_polling()
