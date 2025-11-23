8d51b2a7256d6039da437644911532fc9cd707de
zelenskayaelena44-gif <zelenskaya.elena44@gmail.com>
2025-11-24 00:35:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
+from telegram.ext import Application, CommandHandler
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ContextTypes
 
 BOT_TOKEN = "8270132256:AAELg2DTV0qifcHN4q8uvqrEFy6O75yMPcc"
 
@@ -7,7 +8,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Smart Accessories Bot работает :)")
 
 def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
