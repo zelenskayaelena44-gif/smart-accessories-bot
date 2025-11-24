87681694f96896f2eef9083f7a4060e4b93b1dca
zelenskayaelena44-gif <zelenskaya.elena44@gmail.com>
2025-11-24 01:28:31 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Smart Accessories Bot работает! 😊")
 
 def main():
-    app = Application.builder().token(BOT_TOKEN).build()
+app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
