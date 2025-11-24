26c2347e4c75a7c71585839c7c29fe27422b74d3
zelenskayaelena44-gif <zelenskaya.elena44@gmail.com>
2025-11-24 01:15:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 BOT_TOKEN = "8270132256:AAELg2DTV0qifcHN4q8uvqrEFy6O75yMPcc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Smart Accessories Bot работает :)")
+    await update.message.reply_text("Привет! Smart Accessories Bot работает! 😊")
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
